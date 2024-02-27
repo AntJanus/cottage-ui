562d2d46be2560c058ad2d1fa5d1baa12ad01893
Antonin Januska <antonin@antjanus.com>
2024-02-27 10:23:08 -0700

    add styling export

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
 		rollupOptions: {
 			external: ["react", "react-dom", "react/jsx-runtime"],
 			output: {
+				intro: 'import "./style.css"',
 				globals: {
 					react: "React",
 					"react-dom": "ReactDOM",
